07dda8e693f38dd6fe703897b1271e20bc13a937
Shishir <shishir@192-168-1-104.tpgi.com.au>
2018-07-21 16:44:45 +1000

    toggle test/code ts files

--- run_command_in_docker.py
+++ run_command_in_docker.py
@@ -3,6 +3,7 @@ import re
 import functools
 import sublime
 import string
+import re
 import sublime_plugin
 
 class ShowInPanel:
@@ -48,8 +49,18 @@ class DockerExec(BaseTask):
     command = "docker exec tbb_playpen_"+component_name+"_run_1 "+usr_cmd
     self.run_shell_command(command)
 
-
-
-
-
-
+class ToggleTest(BaseTask):
+  def isEnabled(view, args):
+    return True
+  def run(self, args):
+    file_name_arr = os.path.basename(self.view.file_name()).split(".")
+    if file_name_arr[-2] == 'spec':
+      file_name = file_name_arr[0] + '.ts'
+    else:
+      file_name = file_name_arr[0] + '.spec.ts'
+    new_file_name = os.path.dirname(self.view.file_name()) + "/"+file_name
+    if os.path.exists(new_file_name):
+      self.view.window().open_file(new_file_name)
+    else:
+      create(new_file_name)
+      print('Not Found')
